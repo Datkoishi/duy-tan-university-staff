a4fa8a4200fa058986342ebd08aa9f1cf597c6ab
datkoi211 <truongdinhdetrel@gmail.com>
2025-05-20 15:35:32 +0700

    feat: add Chart component and related subcomponents for improved data visualization

---
+++ components/ui/chart.js
@@ -0,0 +1,36 @@
+// components/ui/chart.js
+
+export class Chart {
+  constructor(element, options) {
+    this.element = element
+    this.options = options
+  }
+
+  update() {
+    // Implement update logic here
+  }
+}
+
+export const ChartContainer = () => {
+  return null // Implement ChartContainer component here
+}
+
+export const ChartTooltip = () => {
+  return null // Implement ChartTooltip component here
+}
+
+export const ChartTooltipContent = () => {
+  return null // Implement ChartTooltipContent component here
+}
+
+export const ChartLegend = () => {
+  return null // Implement ChartLegend component here
+}
+
+export const ChartLegendContent = () => {
+  return null // Implement ChartLegendContent component here
+}
+
+export const ChartStyle = () => {
+  return null // Implement ChartStyle component here
+}
